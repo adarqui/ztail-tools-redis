ab40e7e21f2afc0ad3020d932bff369f6b35f889
Andrew Darqui <andrew.darqui@gmail.com>
2014-10-12 15:39:38 -0400

    multiple queues support, host identifier, host data wrapper

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ ZTail-Tools
 
 Small set of tools which utilize ZTail-Lib, Abstract, and DevUtils. ZTail-Tools will support as many 'queue backends' as are supported by the Abstract library.
 
+Major TODO: ZTail-Lib needs to operate on bytestrings
+
+
 Build
 --
 
@@ -14,5 +17,13 @@ Running
 ---
 
 ```
-cabal run -- ztail-enqueue "redis://localhost:6379" -A -D /var/log/
+./.cabal-sandbox/bin/ztail-enqueue "adarq.org" '["redis://127.0.0.1:6379/key=logs","redis://127.0.0.1:6379/key=broadcast"]' -A -D /var/log/
+```
+
+redis monitor:
+--
+
+```
+1413142609.000790 [0 127.0.0.1:48358] "RPUSH" "logs" "{\"d\":{\"path\":\"/var/log/auth.log\",\"clock\":\"2014-10-12T19:36:49.000Z\",\"tz\":{\"timeZoneName\":\"EDT\",\"timeZoneMinutes\":-240,\"timeZoneSummerOnly\":true},\"buf\":\"Oct 12 15:36:48 polyp sshd[18214]: Connection closed by 127.0.0.1 [preauth]\"},\"h\":\"adarq.org\"}"
+1413142609.000944 [0 127.0.0.1:48359] "RPUSH" "broadcast" "{\"d\":{\"path\":\"/var/log/auth.log\",\"clock\":\"2014-10-12T19:36:49.000Z\",\"tz\":{\"timeZoneName\":\"EDT\",\"timeZoneMinutes\":-240,\"timeZoneSummerOnly\":true},\"buf\":\"Oct 12 15:36:48 polyp sshd[18214]: Connection closed by 127.0.0.1 [preauth]\"},\"h\":\"adarq.org\"}"
 ```
--- tools/ztail-enqueue.hs
+++ tools/ztail-enqueue.hs
@@ -1,25 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, FlexibleInstances #-}
 
 import System.Environment
 
 import ZTail
+import Abstract.Wrapper
 import Abstract.Queue.Enq
+import Control.Monad
+import Data.Aeson
+import Data.Maybe
 
-import qualified Data.ByteString.Char8 as B
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString.Char8 as BSC
 
-usage = "usage: ./ztail-enqueue <queue-url://> <ztail-args...>"
+instance FromJSON (HostDataWrapper TailPacket)
+instance ToJSON (HostDataWrapper TailPacket)
 
-relay rq _ tp = do
- enqueue rq (show tp)
+usage = "usage: ./ztail-enqueue <host-id-string> [<queue-url://>,..] <ztail-args...>"
+
+tp'pack v = lazyToStrict $ encode v
+tp'unpack v = fromJust $ (decode (strictToLazy v) :: Maybe (HostDataWrapper TailPacket))
+
+strictToLazy v = BSL.fromChunks [v]
+lazyToStrict v = BS.concat $ BSL.toChunks v
+encode'bsc v = lazyToStrict $ encode v
+
+relay wrapper rqs _ tp = do
+ mapM_ (\rq -> enqueue rq (wrapper { d = tp })) rqs
 
 main :: IO ()
 main = do
  argv <- getArgs
  case argv of
-  (url:params) -> logger url params
+  (hid:urls:params) -> logger hid (read urls :: [String]) params
   _ -> error usage
 
-logger url params = do
- rq <- mkQueue'Enq url
+logger hid urls params = do
+ rqs <- mapM (\url -> mkQueue'Enq url tp'pack tp'unpack) urls
+ let wrapper = HostDataWrapper { h = hid }
  tails <- parse_args params
- run_main params tailText $ map (\t -> t { ioAct = relay rq}) tails
+ run_main params tailText $ map (\t -> t { ioAct = relay wrapper rqs}) tails
