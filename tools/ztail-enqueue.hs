f49432d670b84ed3b5d6549f9f8dbe39dbac4c1c
Andrew Darqui <andrew.darqui@gmail.com>
2014-10-23 00:32:47 -0400

    ekg for ztail-enqueue

--- tools/ztail-enqueue.hs
+++ tools/ztail-enqueue.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, FlexibleInstances, RecordWildCards #-}
 
 import System.Environment
 
@@ -8,13 +8,24 @@ import Abstract.Queue.Enq
 import Control.Monad
 import Data.Aeson
 import Data.Maybe
+import Data.Int
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Char8 as BSC
 
+import qualified System.Metrics.Distribution as Distribution
+import qualified System.Metrics.Counter as Counter
+import qualified System.Metrics.Gauge as Gauge
+import qualified System.Metrics.Label as Label
+import qualified System.Remote.Monitoring as Monitoring
+
+import ZTail.Tools.EKG
+
 instance FromJSON (HostDataWrapper TailPacket)
 instance ToJSON (HostDataWrapper TailPacket)
+
+port = 60001
 
 usage = "usage: ./ztail-enqueue <host-id-string> [<queue-url://>,..] <ztail-args...>"
 
@@ -25,18 +36,27 @@ strictToLazy v = BSL.fromChunks [v]
 lazyToStrict v = BS.concat $ BSL.toChunks v
 encode'bsc v = lazyToStrict $ encode v
 
-relay wrapper rqs _ tp = do
+relay EKG{..} wrapper rqs _ tp = do
  mapM_ (\rq -> enqueue rq (wrapper { d = tp })) rqs
+ -- stats
+ let len = length $ buf tp
+ Counter.inc _logCounter
+ Gauge.add _lengthGauge (fromIntegral len :: Int64)
+ Distribution.add _logDistribution (fromIntegral len :: Double)
 
 main :: IO ()
 main = do
+ ekg'bootstrap port main'
+
+main' :: EKG -> IO ()
+main' ekg = do
  argv <- getArgs
  case argv of
-  (hid:urls:params) -> logger hid (read urls :: [String]) params
+  (hid:urls:params) -> logger ekg hid (read urls :: [String]) params
   _ -> error usage
 
-logger hid urls params = do
+logger ekg hid urls params = do
  rqs <- mapM (\url -> mkQueue'Enq url tp'pack tp'unpack) urls
  let wrapper = HostDataWrapper { h = hid }
  tails <- parse_args params
- run_main params tailText $ map (\t -> t { ioAct = relay wrapper rqs}) tails
+ run_main params tailText $ map (\t -> t { ioAct = relay ekg wrapper rqs}) tails
